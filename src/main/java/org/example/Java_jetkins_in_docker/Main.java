2058ecf0652f4db9d9b26ecf7bb16ea686bba15f
priya <priyatakale91@gmail.com>
2022-01-09 14:56:27 +0530

    new maven project

---
+++ src/main/java/org/example/Java_jetkins_in_docker/Main.java
@@ -0,0 +1,10 @@
+package org.example.Java_jetkins_in_docker;
+
+public class Main {
+  public static void main(String[] args) {
+    //System.out.println("Hello World!");
+  }
+  public static boolean checkIfInputIsAnEvenNumber(int number){
+	  return number % 2 == 0;
+}
+}
---
+++ src/test/java/org/example/Java_jetkins_in_docker/TestMain.java
@@ -0,0 +1,14 @@
+package org.example.Java_jetkins_in_docker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMain {
+    
+  @Test
+  public void testInputIsEven() {
+	  assertTrue(Main.checkIfInputIsAnEvenNumber(122)); //
+		
+  }
+}
